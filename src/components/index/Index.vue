099978ccb3ac9425803fe4a5d1fb3d94c9ed8a07
shaunyoung-11 <shaun@std.uestc.edu.cn>
2020-09-15 19:23:56 +0800

    准备倒计时功能

--- src/components/index/Index.vue
+++ src/components/index/Index.vue
@@ -1,17 +1,35 @@
 <template>
   <div class="index-container">
     <div class="title1">距招新宣讲</div>
-    <div class="time">13 天 20 时 40 秒</div>
+    <div class="time">
+      <span class="num">{{ days }}</span> 天
+      <span class="num">{{ hours }}</span> 时
+      <span class="num">{{ seconds }}</span> 秒
+    </div>
     <div class="title2">ZeQi Wants You!</div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    console.log(endTime)
+    return {}
+  },
+  methods: {
+    addZero: n => {
+      return n < 10 ? '0' + n : n + ''
+    },
+    countTime: () => {
+      var startTime = new Date()
+      var endTime = new Date('2020/09/25,16:20:00')
+    }
+  }
+}
 </script>
 
 <style scoped>
-.index-container{
+.index-container {
   display: flex;
   flex-direction: column;
   justify-content: space-around;
@@ -20,17 +38,17 @@ export default {}
   min-height: calc(100vh - 61px);
 }
 
-.title1{
+.title1 {
   font-size: 70px;
   font-weight: 700;
 }
 
-.time{
+.time {
   font-size: 120px;
   font-weight: 900;
 }
 
-.title2{
+.title2 {
   font-size: 20px;
   font-weight: 400;
 }
